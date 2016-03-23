75a88eb87394a721caeeb1e9eebe8e15cc1e9b6f
Александр Засим < >
2016-03-23 14:36:46 +0300

    'more'

--- src/pages/lesson/lessonCtrl.js
+++ src/pages/lesson/lessonCtrl.js
@@ -2,4 +2,14 @@ export default function lessonCtrl($state, lessonsConfig) {
     const {sectionId, lessonId} = $state.params;
     this.section = _.findWhere(lessonsConfig, {id: +sectionId});
     this.lesson = _.findWhere(this.section.lessons, {id: +lessonId});
+    this.selectedPart = this.lesson.parts[0];
+    let selectedPartIndex = 0;
+
+    this.nextPart = function () {
+        this.selectedPart = this.lesson.parts[++selectedPartIndex];
+    };
+
+    this.lastPartSelected = function () {
+        return selectedPartIndex === this.lesson.parts.length - 1;
+    };
 }
