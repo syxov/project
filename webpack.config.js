3f4d6ae4b863eea7c3c58bebd24ee66e4e2bc6e3
Александр Засим < >
2016-03-23 10:45:13 +0300

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,6 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
+
 module.exports = {
     context: __dirname + '/src',
     entry: './index',
@@ -5,11 +8,26 @@ module.exports = {
         path: __dirname + '/dist',
         filename: 'bundle.js'
     },
+    resolve: {
+        modulesDirectories: ["node_modules", "bower_components"]
+    },
     loaders: [
         {
             test: /\.js$/,
             loader: 'babel?preset[]=es2015&preset[]=stage-0'
+        },
+        {
+            test: /\.html$/,
+            loader: 'html'
         }
+    ],
+    plugins: [
+        new HtmlWebpackPlugin({
+        template: __dirname + '/src/index.html'
+        }),
+        new webpack.ResolverPlugin(
+            new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin(".bower.json", ["main"])
+        )
     ],
     devServer: {
         contentBase: __dirname + '/dist'
