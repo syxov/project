7c90114096a5fd7074612b123a225247706ed331
Александр Засим < >
2016-03-26 19:30:57 +0300

    'update'

--- src/config/lessonsConfig/lessonsConfig.js
+++ src/config/lessonsConfig/lessonsConfig.js
@@ -1,15 +1,15 @@
 export default [
     {
         id: 1,
-        sectionName: 'first',
+        sectionName: 'Введение в психологию',
         lessons: [
             {
                 id: 1,
-                lessonName: 'firstLesson',
+                lessonName: 'Общее представление о психологии как о науке',
                 parts: [
                     {
                         type: 'theory',
-                        value: 'sdf'
+                        value: require('./configs/introduceToPsihology.html')
                     },
                     {
                         type: 'controlQuestions',
@@ -31,6 +31,118 @@ export default [
                                 answers: ['fuck']
                             }
                         ]
+                    }
+                ]
+            },
+            {
+                id: 2,
+                lessonName: 'Методы исследования в психологии',
+                parts: [
+                    {
+                        type: 'theory',
+                        value: require('./configs/metods.html')
+                    }
+                ]
+            },
+            {
+                id: 3,
+                lessonName: 'Происхождение и развитие сознания человека',
+                parts: [
+                    {
+                        type: 'theory',
+                        value: require('./configs/theDevelopmentMind.html')
+                    }
+                ]
+            },
+            {
+                id: 4,
+                lessonName: 'Психологическая теория деятельности',
+                parts: [
+                    {
+                        type: 'theory',
+                        value: require('./configs/activityTheory.html')
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        id: 2,
+        sectionName: 'Психология познавательных процессов',
+        lessons: [
+            {
+                id: 1,
+                lessonName: 'Внимание',
+                parts: [
+                    {
+                        type: 'theory',
+                        value: require('./configs/attention.html')
+                    }
+                ]
+            },
+            {
+                id: 2,
+                lessonName: 'Ощущения',
+                parts: [
+                    {
+                        type: 'theory',
+                        value: require('./configs/feeling.html')
+                    }
+                ]
+            },
+            {
+                id: 3,
+                lessonName: 'Восприятие',
+                parts: [
+                    {
+                        type: 'theory',
+                        value: require('./configs/perception.html')
+                    }
+                ]
+            },
+            {
+                id: 4,
+                lessonName: 'Память',
+                parts: [
+                    {
+                        type: 'theory',
+                        value: require('./configs/memory.html')
+                    }
+                ]
+            },
+            {
+                id: 5,
+                lessonName: 'Мышление и речь',
+                parts: [
+                    {
+                        type: 'theory',
+                        value: require('./configs/thoughtAndSpeech.html')
+                    }
+                ]
+            },
+            {
+                id: 6,
+                lessonName: 'Воображение',
+                parts: [
+                    {
+                        type: 'theory',
+                        value: require('./configs/imagination.html')
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        id: 3,
+        sectionName: 'Психология личности',
+        lessons: [
+            {
+                id: 1,
+                lessonName: 'Введение в психологию личности',
+                parts: [
+                    {
+                        type: 'theory',
+                        value: require('./configs/introductionToTheIndividualPsychology.html')
                     }
                 ]
             }
--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,14 @@ module.exports = {
                 loaders: ['style', 'css']
             },
             { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
-            { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
+            { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" },
+            {
+                test: /\.(jpe?g|png|gif)$/i,
+                loaders: [
+                    'file',
+                    'image-webpack'
+                ]
+            }
         ]
     },
     plugins: [
