3b14fd1399475daa07e32219d477ffc4b9a86a65
Александр Засим < >
2016-03-29 19:26:44 +0300

    update

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,10 @@ module.exports = {
                 test: /\.css$/,
                 loaders: ['style', 'css']
             },
+            {
+                test: /\.less/,
+                loaders: ['style', 'css', 'less']
+            },
             { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
             { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" },
             {
