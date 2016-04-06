5cb68db8e6c148be8642a2ea84562c5249a01381
Александр Засим < >
2016-04-06 21:20:03 +0300

    Update

--- src/components/controlQuestions/controlQuestionsCtrl.js
+++ src/components/controlQuestions/controlQuestionsCtrl.js
@@ -74,6 +74,51 @@ export default function controlQuestionsCtrl() {
     };
 
     function isCorrectAnswer(userAnswer = '', realAnswer) {
-        return userAnswer.trim().toLowerCase() === realAnswer.trim().toLowerCase();
+        const lowerCasedUserAnswer = userAnswer.trim().toLowerCase();
+        const lowerCasedRealAnswer = realAnswer.trim().toLowerCase();
+        return levenshtein(lowerCasedUserAnswer, lowerCasedRealAnswer) <= 2;
+    }
+
+    /**
+     * @param {string} s1 Исходная строка
+     * @param {string} s2 Сравниваемая строка
+     * @return {number} Расстояние Левенштейна
+     */
+    function levenshtein(s1, s2) {
+        let i, j, l1, l2, flip, ch, chl, ii, ii2, cost, cutHalf;
+        l1 = s1.length;
+        l2 = s2.length;
+
+        const cr = 1;
+        const cri = 1;
+        const ci = 1;
+        const cd = 1;
+
+        cutHalf = flip = Math.max(l1, l2);
+
+        const minCost = Math.min(cd, ci, cr);
+        const minD = Math.max(minCost, (l1 - l2) * cd);
+        const minI = Math.max(minCost, (l2 - l1) * ci);
+        const buf = new Array((cutHalf * 2) - 1);
+
+        for (i = 0; i <= l2; ++i) {
+            buf[i] = i * minD;
+        }
+
+        for (i = 0; i < l1; ++i, flip = cutHalf - flip) {
+            ch = s1[i];
+            chl = ch.toLowerCase();
+
+            buf[flip] = (i + 1) * minI;
+
+            ii = flip;
+            ii2 = cutHalf - flip;
+
+            for (j = 0; j < l2; ++j, ++ii, ++ii2) {
+                cost = (ch === s2[j] ? 0 : (chl === s2[j].toLowerCase()) ? cri : cr);
+                buf[ii + 1] = Math.min(buf[ii2 + 1] + cd, buf[ii] + ci, buf[ii2] + cost);
+            }
+        }
+        return buf[l2 + cutHalf - flip];
     }
 }
