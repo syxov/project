50b88271cf1795be399b98092d27de8214cbbbd2
Александр Засим < >
2016-04-09 21:34:31 +0300

    Update

--- src/components/test/testCtrl.js
+++ src/components/test/testCtrl.js
@@ -8,19 +8,21 @@ export default function testCtrl($scope, $uibModal) {
 
     this.endTest = function () {
         if (this.userAnsweredOnAllQuestions()) {
-            this.questionPart = false;
-            $scope.$emit('testEnded');
+            this._reallyEndTest();
         } else {
             $uibModal.open({
                 template: require('./popup/popup.html'),
                 bindToController: true,
                 controllerAs: '$ctrl',
                 controller: 'popupCtrl'
-            }).result.then(() => {
-                this.questionPart = false;
-                $scope.$emit('testEnded');
-            });
+            }).result.then(::this._reallyEndTest);
         }
+    };
+
+    this._reallyEndTest = function () {
+        this.questionPart = false;
+        this.percent = this._computePercent();
+        $scope.$emit('testEnded');
     };
 
     this.isLastQuestionSelected = function () {
@@ -37,6 +39,20 @@ export default function testCtrl($scope, $uibModal) {
 
     this.userAnsweredSmth = function (question) {
         return question.answers.some(({ userSelect }) => userSelect);
+    };
+
+    this._computePercent = function () {
+        const result = this._computeCorrectAnswersCount() / this.questions.length * 100;
+        return Number.isFinite(result) ? result : 0;
+    };
+
+    this._computeCorrectAnswersCount = function () {
+        return this.questions.filter(function (question) {
+            return question.answers.every(function (answer) {
+                return (answer.userSelect === answer.isCorrect && answer.isCorrect === true) ||
+                    (!answer.isCorrect && !answer.userSelect)
+            });
+        }).length;
     };
 
     $scope.$watch(
